8385e0f2a4fd2ee5c3c408b622d44f9f36de7714
ZhangChi <jeorch@foxmail.com>
2018-07-09 10:22:13 +0800

    update

--- js/max_data_mapreduce_job.js
+++ js/max_data_mapreduce_job.js
@@ -1,64 +1,34 @@
+function getSalesAgg(collection, scope){
+    db[collection].mapReduce(
+            function() { emit(this.MARKET, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { MARKET: { $exists: true } },
+                out: {
+                    replace: collection + scope,
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+}
+
+function getCompanySalesAgg(collection, scope){
+    db[collection].mapReduce(
+            function() { emit(this.MARKET, this.f_sales); },
+            function(key, values) { return Array.sum(values); }, {
+                query: { belong2company: 1 },
+                out: {
+                    replace: collection + scope,
+                    db: "pharbers-max-aggregation"
+                }
+            }
+        );
+}
+
 db.getCollectionNames().forEach(
-    function(collection) {
-        db[collection].mapReduce(
-            function() { emit(this.MARKET, this.f_sales); },
-            function(key, values) { return Array.sum(values); }, {
-                query: { MARKET: { $exists: true } },
-                out: {
-                    replace: collection + "_NATION_SALES",
-                    db: "pharbers-max-aggregation"
-                }
-            }
-        );
-        db[collection].mapReduce(
-            function() { emit(this.MARKET, this.f_sales); },
-            function(key, values) { return Array.sum(values); }, {
-                query: { belong2company: 1 },
-                out: {
-                    replace: collection + "_NATION_COMPANY_SALES",
-                    db: "pharbers-max-aggregation"
-                }
-            }
-        );
-        db[collection].mapReduce(
-            function() { emit(this.Province, this.f_sales); },
-            function(key, values) { return Array.sum(values); }, {
-                query: { MARKET: { $exists: true } },
-                out: {
-                    replace: collection + "_PROVINCE_SALES",
-                    db: "pharbers-max-aggregation"
-                }
-            }
-        );
-        db[collection].mapReduce(
-            function() { emit(this.Province, this.f_sales); },
-            function(key, values) { return Array.sum(values); }, {
-                query: { belong2company: 1 },
-                out: {
-                    replace: collection + "_PROVINCE_COMPANY_SALES",
-                    db: "pharbers-max-aggregation"
-                }
-            }
-        );
-        db[collection].mapReduce(
-            function() { emit(this.City, this.f_sales); },
-            function(key, values) { return Array.sum(values); }, {
-                query: { MARKET: { $exists: true } },
-                out: {
-                    replace: collection + "_CITY_SALES",
-                    db: "pharbers-max-aggregation"
-                }
-            }
-        );
-        db[collection].mapReduce(
-            function() { emit(this.City, this.f_sales); },
-            function(key, values) { return Array.sum(values); }, {
-                query: { belong2company: 1 },
-                out: {
-                    replace: collection + "_CITY_COMPANY_SALES",
-                    db: "pharbers-max-aggregation"
-                }
-            }
-        );
-    }
+    getSalesAgg(collection, "_NATION_SALES");
+    getSalesAgg(collection, "_PROVINCE_SALES");
+    getSalesAgg(collection, "_CITY_SALES");
+    getCompanySalesAgg(collection, "_NATION_COMPANY_SALES");
+    getCompanySalesAgg(collection, "_PROVINCE_COMPANY_SALES");
+    getCompanySalesAgg(collection, "_CITY_COMPANY_SALES");
 );
